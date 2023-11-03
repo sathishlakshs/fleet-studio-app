259b08729a121162c799d45e5b6d1b4e8935d4a1
sathishlakshs <sathishkumar3110@gmail.com>
2023-11-03 17:29:10 +0530

    search color by given word is done

--- src/App.js
+++ src/App.js
@@ -6,22 +6,36 @@ import { useEffect } from 'react';
 function App() {
 
   const [list, setList] = React.useState([]);
+  const [colors, setColors] = React.useState();
+  const [value, setValue] = React.useState('');
   const handleChange = (v) => {
-    const data = colorList.filter(item => (item.name.includes(v) || item.hex.includes(v)));
-    setList(data);
+    setValue(v);
+    // const data = colorList.filter(item => (item.name.includes(v) || item.hex.includes(v)));
+    // setList(data);
+  }
+  
+  const onSearch = (event) => {
+    if(event.key === 'Enter') {
+      if(value) {
+        const temp = colors[value];
+        setList(temp?[...temp]:null);
+        return;
+      }
+      setList(null);
+    }
   }
 
   useEffect(() => {
-    setList(colorList);
+    setColors(colorList);
   }, []);
 
   return (
     <div className="App">
-      <input type="text" style={{width:'80%'}} onChange={(e) => handleChange(e.target.value)}/>
+      <input type="text" className='searchBox' style={{width:'80%'}} onChange={(e) => handleChange(e.target.value)} onKeyDown={onSearch} value={value}/>
       <div className="container" >{
-      list.map(item => {
-        return <div className={'card'}  style={{'backgroundColor': item.hex}} key={item.hex}>{item.name}</div>
-      })}
+      list?list.map(item => <div className={'card'}   key={item.hexCode}><div style={{'backgroundColor': item.hexCode, height: '180px'}}></div><p>{`${item.hexCode}`}</p></div>):
+    'Your search phrase related data not found in data.js'
+    }
       </div>
     </div>
   );
